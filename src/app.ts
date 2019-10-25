6cdb13223eee4a614cc5c78858c6a45849a6f9d6
Yuki Takenaka <yuki.takenaka@andco.group>
2019-10-25 15:05:05 +0900

    Delete extra files

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,8 @@
-// import bootstrap from './bootstrap';
 import Vue from 'vue';
-
 import router from './router';
 import App from './App.vue';
 
 import './assets/style/main.scss';
-
-// bootstrap();
 
 new Vue({
   el: '#app',
